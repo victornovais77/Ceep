b34eb71dcd91b2e9c0450e6755863370293303cc
victornovais77 <victornovai77@gmail.com>
2018-07-16 14:00:57 -0300

    Funcionalidades com Progressive Enhancement

--- js/opçoesDaPagina/btnMudaLayout.js
+++ js/opçoesDaPagina/btnMudaLayout.js
@@ -8,11 +8,14 @@ function mudaTexto() {
     }
 }
 
-btn.onclick = mudaTexto;
+btn.addEventListener('click',mudaTexto);
 
 const mural = document.querySelector('.mural');
+
 function mudaLayout(){
     mural.classList.toggle('mural--linha');
 }
 
 btn.addEventListener('click',mudaLayout);
+
+btn.classList.remove('no-js')
