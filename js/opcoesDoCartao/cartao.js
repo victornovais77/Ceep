0ce7e04fbb20429d8df107ac0ce2cb4611a96d6e
victornovais77 <victornovai77@gmail.com>
2018-07-17 14:46:59 -0300

    Ajustando a Navegação via Teclado

--- js/opcoesDoCartao/cartao.js
+++ js/opcoesDoCartao/cartao.js
@@ -1,4 +1,4 @@
-;(function () {
+; (function () {
 
     const cartoes = document.querySelectorAll('.cartao')
 
@@ -11,18 +11,28 @@
         cartao.addEventListener('focusout', function () {
             cartao.classList.remove("cartao--focado")
         })
-        
-        
+
+
         cartao.addEventListener('change', function (event) {
-    
+
             const elementoClicado = event.target;
-    
+
             const isRadioTipo = elementoClicado.classList.contains('opcoesDoCartao-radioTipo');
-    
+
             if (isRadioTipo) {
                 console.log(isRadioTipo, elementoClicado.value)
                 const corNovaDoCartao = elementoClicado.value
                 cartao.style.backgroundColor = corNovaDoCartao
+            }
+        })
+
+        cartao.addEventListener('keypress', function (event) {
+            const ElementoAtivo = event.target;
+
+            const isOpcaoDoCartao = ElementoAtivo.classList.contains('opcoesDoCartao-opcao')
+
+            if (isOpcaoDoCartao && (event.key == 'Enter' || ' ')) {
+                ElementoAtivo.click();
             }
         })
 
